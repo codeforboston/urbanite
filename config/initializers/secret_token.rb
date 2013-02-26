bd46a6fb04ebaaf4c95383cd1e26d87ca41be2d1
Roberto Segebre <robertosegebre13@gmail.com>
2013-02-25 20:09:16 -0500

    Generated blank new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urbanite::Application.config.secret_token = '0a8059bc00bdde38a4f026af79eddfd16353645c0032f1d1afd7a9d1b6ad370d61dcaf087c4cf3eb20d1cd4be0d6d5a5c879800b819a3c0c243a50cc250a51f8'
