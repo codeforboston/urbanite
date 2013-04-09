b7bce0381e538242d27c536f0a9c6a14556eecb0
Roberto Segebre <rsegebre@colgate.edu>
2013-04-08 22:12:47 -0400

    Added basic home page and made root page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+	@title = "Urbanite"
+	@intro_mssg = "A Web App that allows users to access 'hidden' cultural gems in their local environment by leveraging social media streams from local venues and cultural organizations like bands, artists, authors, chefs, etc."
+	@layout = ["./others.html"]
   end
 end
