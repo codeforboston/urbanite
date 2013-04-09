bf6d0f72c4190896f67acb0a55a770403c3f3cb5
Roberto Segebre <rsegebre@colgate.edu>
2013-04-08 21:28:08 -0400

    Removed default index page, generate dummy home page

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,10 @@ module Urbanite
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     #config.active_record.whitelist_attributes = true
-
+	config.generators do |g|
+		g.orm :mongo_mapper
+	end
+	
     # Enable the asset pipeline
     config.assets.enabled = true
 
