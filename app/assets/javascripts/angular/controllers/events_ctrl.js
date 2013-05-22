74c7125232d2147f65f9ae26cd9e658a9e885be3
josmas <josmasflores@gmail.com>
2013-05-21 22:08:43 -0400

    cleaning up TODO items already done

--- app/assets/javascripts/angular/controllers/events_ctrl.js
+++ app/assets/javascripts/angular/controllers/events_ctrl.js
@@ -15,7 +15,5 @@ var EventsCtrl = function ($scope, $http) {
 
 events.controller('EventsCtrl', EventsCtrl);
 
-
-//TODO (Jose) test that this works now that this is a module
 // Avoid minification of the injected dependencies by the assets pipeline
 EventsCtrl.$inject = ['$scope', '$http'];
