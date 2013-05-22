cd082d7810001f21cc40136280bc42c5ebe79dc6
beechnut <cloydster@gmail.com>
2013-05-21 22:37:38 -0400

    Add updated Angular from Jos's commit

--- app/assets/javascripts/angular_app.js
+++ app/assets/javascripts/angular_app.js
@@ -1,4 +1,24 @@
-var urbanite = angular.module('urbanite', ['ngResource'])
-  .config(["$httpProvider", function(provider) {
-    provider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-  }])
+/**
+ * License goes here
+ */
+
+'use strict';
+
+/**
+ * Angular needs to send the Rails CSRF token with each post request.
+ *
+ * Here we get the token from the meta tags (make sure <%= csrf_meta_tags %>
+ * is present in your layout.)
+ */
+angular.module('urbanite', ['events']).
+  // configure our http requests to include the Rails CSRF token
+  config(["$httpProvider", function (p) {
+    var m = document.getElementsByTagName('meta'),
+      i;
+    for (i in m) {
+      if (m[i].name === 'csrf-token') {
+        p.defaults.headers.common['X-CSRF-Token'] = m[i].content;
+        break;
+      }
+    }
+  }]);
