ead7d28d007fba801fe6fc061af3050ea1da9629
beechnut <cloydster@gmail.com>
2013-05-24 16:17:03 -0400

    Configure rspec to work with mongoid

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,7 +30,8 @@ Urbanite::Application.configure do
   config.action_mailer.delivery_method = :test
 
   # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
+  # config.active_record.mass_assignment_sanitizer = :strict
+  # Commented out to prevent use of ActiveRecord
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
