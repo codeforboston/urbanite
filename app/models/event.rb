58e93e31ce785afb127c16e36761870a6f79da12
beechnut <cloydster@gmail.com>
2013-05-26 14:16:29 -0400

    Add tests to reflect Event, Venue Templates v3

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,26 +4,35 @@ class Event
   field :name,        type: String
   field :description, type: String
   field :categories,  type: Array
+  field :urls,        type: Array
   
+  embeds_one :schedule
+  has_many :venues, validate: false
+  recursively_embeds_many
+  embeds_many :third_parties
+  accepts_nested_attributes_for :third_parties
+
+  field :organizations, type: Array
   field :thumbnail_url, type: String
   field :status,        type: Boolean,  default: true
-
-  # RELATION has_one :venue =>
-  embeds_one :schedule
-
-  # field :third_party, type: Array #Information regarding the event from a third_party 
-  # field :organizations, type: Array #Array containing the organizations participating in the event
+  field :public,        type: Boolean,  default: true
   
 end
+
 
 class Schedule
   include Mongoid::Document
   field :date, type: String
   field :start_time, type: String
   field :end_time,   type: String
+
   embedded_in :event
 
   validates :date, presence: true
   validates :start_time, presence: true
 
+end
+
+class ThirdParty
+  include Mongoid::Document
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,13 +1,29 @@
 require 'spec_helper'
 
 describe Event do
-  
+
+  it { should have_field :_id }  
   it { should have_fields(:name, :description).of_type(String) }
   it { should have_field(:categories).of_type(Array) }
-  it { should have_field(:thumbnail_url).of_type(String) }
-  it { should have_field(:status).of_type(Boolean) }
 
   it { should embed_one :schedule }
+  it { should have_many :venues }
+  # it { should recursively_embed_many }
+
+  it { should have_field(:urls).of_type(Array) }
+  
+  it { should embed_many :third_parties }
+  it { should accept_nested_attributes_for :third_parties }
+
+  it { should have_field(:organizations).of_type(Array) }
+  it { should have_field(:thumbnail_url).of_type(String) }
+  it { should have_field(:status).of_type(Boolean) }
+  it { should have_field(:public).of_type(Boolean) }
+
+  it { should have_field(:created_at, :updated_at).of_type(Time) }
+
+
+  it { should_not validate_associated :venues } 
 
 
 end
